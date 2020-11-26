0ff255faf515c3669cc181f9faf17d6f33ff5d42
LeandroRD <63479279+LeandroRD@users.noreply.github.com>
2020-11-26 14:00:06 -0300

    Add files via upload

---
+++ SPACET/_cabecalho.php
@@ -0,0 +1,26 @@
+<?php
+//===========================================
+//cabecalho
+//===========================================
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SPACET</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" >
+    <!--  link para Font Awesome -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- link css -->
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+   
+
+
---
+++ SPACET/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="inc/main.css">
+</head>
+<body>
+
+<?php
+//===========================================
+//
+//===========================================
+include_once('_cabecalho.php');
+
+echo'<p>teste Awesome <span class="fa fa-times"></spam></p>';
+
+
+include_once('_rodape.php');
+
+?>
+    
+</body>
+</html>
+
